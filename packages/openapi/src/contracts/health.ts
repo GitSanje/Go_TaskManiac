ab8263f8e33b7dd8ecf1b722a4033b1eb0a21044
Sanjay <karkisanjay2002@gmail.com>
2025-09-17 20:44:33 +0545

    added css, and comment out the err

--- packages/openapi/src/contracts/health.ts
+++ packages/openapi/src/contracts/health.ts
@@ -1,7 +1,6 @@
 import { initContract } from "@ts-rest/core";
 import { z } from "zod";
 import { ZHealthResponse } from "@taskManiac/zod";
-import { getSecurityMetadata } from "@/utils.js";
 
 const c = initContract();
 
